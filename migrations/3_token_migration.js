362cfc5c53f9b33a90343881ec09ee47b7233cb7
danieljoseph18 <88494797+danieljoseph18@users.noreply.github.com>
2021-12-27 15:28:28 +0000

    Create 3_token_migration.js

---
+++ migrations/3_token_migration.js
@@ -0,0 +1,7 @@
+const Link = artifacts.require("Link");
+const Wallet = artifacts.require("Wallet");
+
+module.exports = async function (deployer,  network, accounts) {
+  await deployer.deploy(Link);
+  await deployer.deploy(Wallet);
+};
